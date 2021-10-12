e2ca70656db8b7432f42246e669f142988f7ab8d
Ishant Bhurani <ishantbhurani@gmail.com>
2021-10-12 22:41:59 +0530

    Add logic to count valid words

--- js/app.js
+++ js/app.js
@@ -2,3 +2,19 @@
 
 // - write the JS so that the word count appears on the screen
 // - update the count as the user types
+
+const textEl = document.getElementById("txtid");
+const wordCountEl = document.getElementById("count");
+
+function updateCount() {
+  const text = this.value.trim();
+  if (text) {
+    wordCountEl.textContent = text
+      .split(/\s+/)
+      .filter((word) => /[A-Za-z]+/.test(word)).length;
+  } else {
+    wordCountEl.textContent = "0";
+  }
+}
+
+textEl.addEventListener("input", updateCount);
